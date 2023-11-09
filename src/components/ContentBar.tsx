26dc0e2e959a4535ce375eb5ae880fc70bf58c6e
Tarik Yurtseven <devtarikyurts@gmail.com>
2023-11-09 15:58:13 +0300

    active content bar added

--- src/components/ContentBar.tsx
+++ src/components/ContentBar.tsx
@@ -5,13 +5,16 @@ import { LastMatches } from './LastMatches'
 import { Statistics } from './Statistics'
 import Teammates from './Teammates'
 import { motion } from 'framer-motion'
+import { useState } from 'react'
 
 export const ContentBar = () => {
 
-    const { setContent, content} = useApp();
+    const { setContent} = useApp();
+    const [active, setActive] = useState('LastMatches')
 
-    const handleClick = (content: JSX.Element) => {
+    const handleClick = (content: JSX.Element, contentName: string) => {
         setContent(content)
+        setActive(contentName)
     }
 
     return (
@@ -22,9 +25,9 @@ export const ContentBar = () => {
             animate={{ opacity: 1, scale: 1 }}
             exit={{ opacity: 0, scale: 0 }}
             className='w-full h-full flex justify-center items-center border rounded-md text-[#FFD700] border-[#FFD700] hover:cursor-pointer'>
-            <div className={`flex justify-center items-center border-r ${content.type.name === 'LastMatches' ? 'bg-[#FFD700] text-black' : 'bg-none'} border-[#FFD700] w-1/3 h-full transition hover:bg-[#FFD700] hover:text-[#000]`} onClick={() => handleClick(<LastMatches />)}><FaClockRotateLeft /></div>
-            <div className={`flex justify-center items-center border-r ${content.type.name === 'Statistics' ? 'bg-[#FFD700] text-black' : 'bg-none'} border-[#FFD700] w-1/3 h-full transition hover:bg-[#FFD700] hover:text-[#000]`} onClick={() => handleClick(<Statistics />)}><FaCrosshairs /></div>
-            <div className={`flex justify-center items-center border-r ${content.type.name === 'Teammates' ? 'bg-[#FFD700] text-black' : 'bg-none'} border-[#FFD700] w-1/3 h-full transition hover:bg-[#FFD700] hover:text-[#000]`} onClick={() => handleClick(<Teammates />)}><FaUserFriends /> </div>
+            <div className={`flex justify-center items-center border-r ${active === 'LastMatches' ? 'bg-[#FFD700] text-black' : 'bg-none'} border-[#FFD700] w-1/3 h-full transition hover:bg-[#FFD700] hover:text-[#000]`} onClick={() => handleClick(<LastMatches />, 'LastMatches')}><FaClockRotateLeft /></div>
+            <div className={`flex justify-center items-center border-r ${active === 'Statistics' ? 'bg-[#FFD700] text-black' : 'bg-none'} border-[#FFD700] w-1/3 h-full transition hover:bg-[#FFD700] hover:text-[#000]`} onClick={() => handleClick(<Statistics />, 'Statistics')}><FaCrosshairs /></div>
+            <div className={`flex justify-center items-center border-r ${active === 'Teammates' ? 'bg-[#FFD700] text-black' : 'bg-none'} border-[#FFD700] w-1/3 h-full transition hover:bg-[#FFD700] hover:text-[#000]`} onClick={() => handleClick(<Teammates />, 'Teammates')}><FaUserFriends /> </div>
 
         </motion.div>
     )
